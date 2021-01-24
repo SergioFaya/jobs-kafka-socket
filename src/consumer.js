b8953a57f8f122cfb7809486a8cac200e6e2e9aa
Sergio Faya <sergiofayafernandez@gmail.com>
2021-01-24 02:17:33 +0100

    temporal socket test

--- src/consumer.js
+++ src/consumer.js
@@ -1,15 +1,15 @@
 const { Kafka } = require('kafkajs')
 
-const clientId = 'socket-app';
-const brokers = ['localhost:9092']
+const clientId = process.env.KAFKA_CLIENT_ID;
+const brokers = [process.env.KAFKA_BROKER];
 
 const kafka = new Kafka({
 	clientId,
 	brokers
 });
 
-const consumerGroupId = 'groupId';
-const topic = "topic1";
+const consumerGroupId = process.env.KAFKA_CONSUMER_GROUP_ID
+const topic = process.env.CONSUMER_TOPIC;
 
 module.exports = async () => {
 	const consumer = kafka.consumer({ groupId: consumerGroupId });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-const app = require("express")()
-require('dotenv').config();
+// const app = require("express")()
+// const http = require('http').Server(app);
+// require('dotenv').config();
 
 
-const PORT = 8080;
-const init = async () => {
-	const server = await app.listen(PORT, () => console.log('Listening on http://localhost:3000'))
-	const consumer = await require("./consumer")();
-	// Create a socket.io instance
-	await require('./sockets')(server, consumer)
-}
-init()
+// const PORT = process.env.PORT;
+
+// const init = async () => {
+// 	const consumer = await require("./consumer")();
+// 	// Create a socket.io instance
+// 	const io = require('socket.io')(http);
+
+// 	io.on('connection', (socket) => {
+// 		console.log("A user connected", socket);
+// 		socket.on('disconnect', () => {
+// 			console.log("A user disconnected");
+// 		});
+
+// 		consumer.run({
+// 			eachMessage: async ({ topic, partition, message }) => {
+// 				console.log(io)
+// 				io.emit(topic, message)
+// 				console.log({ topic, partition, message, messageContent: message.value.toString() });
+// 			},
+// 		});
+
+// 	});
+// 	await app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+// }
+// init()
+
+// Without server config
+// const io = require('socket.io')();
+// io.on('connection', client => {
+// 	console.log("connection")
+// });
+// io.listen(3000);
+
+
+const options = {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"]
+	}
+};
+const io = require('socket.io')(options);
+
+io.on('connection', socket => {
+	console.log("connected", socket)
+});
+
+io.listen(3000);

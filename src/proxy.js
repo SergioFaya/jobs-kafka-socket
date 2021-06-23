6cd2ff2fd8cd63d4efa1b07e209a2da49ab86af5
Sergio Faya <sergiofayafernandez@gmail.com>
2021-06-23 23:44:34 +0200

    graphql update

--- src/proxy.js
+++ src/proxy.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const axios = require('axios');
 
 const PORT_PROXY = process.env.PORT_PROXY;
-const PROCESSOR_HOST = process.env.PROCESSOR_HOST;
+const PROCESSOR_HOSTS = process.env.PROCESSOR_HOST.split(',');
 
 
 app.use(function (req, res, next) {
@@ -12,71 +12,74 @@ app.use(function (req, res, next) {
 	next();
 });
 
+const getFromProcessors = async () => {
+	console.log("hosts", PROCESSOR_HOSTS)
+
+	const processor1Response = await axios.get(PROCESSOR_HOSTS[0]);
+	const processor2Response = await axios.get(PROCESSOR_HOSTS[1]);
+
+	return [...processor1Response.data, ...processor2Response.data];
+}
+
 app.get('/', async function (_req, res) {
-	axios
-		.get(PROCESSOR_HOST)
+	getFromProcessors()
 		.then(function (response) {
-			res.send(response.data);
+			res.send(response);
 		})
 		.catch(function (error) {
-			res.send(error);
+			res.status(500).send('Get all failed. ' + error);
 		});
 });
 
 app.get('/tags/:name', async function (req, res) {
 	const tagname = req.params.name;
-	axios
-		.get(PROCESSOR_HOST)
+	getFromProcessors()
 		.then(function (response) {
-			const jobs = response.data;
+			const jobs = response;
 			const filtered = jobs.filter(job => job.tags.includes(tagname));
 			res.send(filtered);
 		})
 		.catch(function (error) {
-			res.send(error);
+			res.status(500).send('Get tags failed. ' + error);
 		});
 });
 
 app.get('/content/:value', async function (req, res) {
 	const value = req.params.value.toUpperCase();
-	axios
-		.get(PROCESSOR_HOST)
+	getFromProcessors()
 		.then(function (response) {
-			const jobs = response.data;
-			const filtered = jobs.filter(job =>
-				job.description.toUpperCase().includes(value)
-				|| job.company.toUpperCase().includes(value)
-				|| job.title.toUpperCase().includes(value)
-				|| job.location.country.toUpperCase().includes(value)
-				|| job.location.city.toUpperCase().includes(value)
+			const jobs = response;
+			const filtered = jobs.filter(job => {
+				const isInDescription = job.description.toUpperCase().includes(value)
+				const isInCompany = job.company.toUpperCase().includes(value)
+				const isInTitle = job.title.toUpperCase().includes(value)
+				return isInDescription || isInCompany || isInTitle
+			}
 			);
 			res.send(filtered);
 		})
 		.catch(function (error) {
-			res.send(error);
+			res.status(500).send('Get content failed. ' + error);
 		});
 });
 
 app.get('/salary/:amount', async function (req, res) {
 	const amount = parseInt(req.params.amount);
-
-	axios
-		.get(PROCESSOR_HOST)
+	getFromProcessors()
 		.then(function (response) {
-			const jobs = response.data;
+			const jobs = response;
 			const filtered = jobs.filter(job => job.has_salary && amount > job.salary.low);
 			res.send(filtered);
 		})
 		.catch(function (error) {
-			res.send(error);
+			res.status(500).send('Get salary failed. ' + error);
 		});
 });
 
 app.get('/charts/bar', async function (req, res) {
-	axios
-		.get(PROCESSOR_HOST)
+	getFromProcessors()
 		.then(function (response) {
-			const jobs = response.data;
+			const jobs = response;
 			const tags = jobs
 				.flatMap(job => job.tags)
 			const groupedObject = groupBy(tags)
@@ -86,7 +89,24 @@ app.get('/charts/bar', async function (req, res) {
 			res.send(result);
 		})
 		.catch(function (error) {
-			res.send(error);
+			res.status(500).send('Get charts failed. ' + error);
+		});
+});
+
+app.get('/tags', async function (req, res) {
+	getFromProcessors()
+		.then(function (response) {
+			const jobs = response;
+			const tags = jobs
+				.flatMap(job => job.tags)
+			const groupedObject = groupBy(tags)
+			const result = Object.entries(groupedObject).map(([key, value]) => {
+				return key
+			})
+			res.send(result);
+		})
+		.catch(function (error) {
+			res.status(500).send('Get charts failed. ' + error);
 		});
 });
 
